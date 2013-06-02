2a012f238a037e76d5e6b168bf6398d4cc03a808
jasti <vamseej@gmail.com>
2013-06-02 13:16:29 -0400

    logos

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130527160841) do
+ActiveRecord::Schema.define(:version => 20130601234253) do
 
   create_table "categories", :force => true do |t|
     t.string   "category_name"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
+
+  create_table "posts", :force => true do |t|
+    t.string   "question"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "posts", ["user_id", "created_at"], :name => "index_posts_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
